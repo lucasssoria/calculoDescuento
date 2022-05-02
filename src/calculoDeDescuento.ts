c3b0b906aafa61991ddf2329b7877cac5cd39ee3
Lucas Soria <104267584+lucasssoria@users.noreply.github.com>
2022-05-02 09:37:30 -0300

    Initial commit

---
+++ src/calculoDeDescuento.ts
@@ -0,0 +1,5 @@
+let producto1: number = 450.5;
+let descuento: number = (10 / 100) * producto1;
+let productoFinal: number = producto1 - descuento;
+
+console.log("El precio final es: ", productoFinal);
